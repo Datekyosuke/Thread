0ce6fd60445de1c94e088a0ce1492171654a41c6
Yury Kuzmin <rakoot@yandex.ru>
2024-03-28 22:05:19 +0300

    TaskStatus

---
+++ TaskStatus/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+
+    private static void Main()
+    {
+        Task task = new Task(new Action(Method));
+
+        Console.WriteLine($"{task.Status}");
+
+        task.Start();
+        Console.WriteLine($"{task.Status}");
+        Thread.Sleep( 1000 );
+
+        Console.WriteLine($"{task.Status}");
+        Thread.Sleep(2000);
+
+        Console.WriteLine($"{task.Status}");
+        Thread.Sleep(1000);
+
+    }
+
+    private static void Method()
+    { 
+        Thread.Sleep(2000);
+    }
+}
