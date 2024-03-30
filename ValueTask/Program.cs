346c5791be9a167490bb2fdcdf5964bc862ac0da
Yury Kuzmin <rakoot@yandex.ru>
2024-03-30 20:59:23 +0300

    ValueTask

---
+++ ValueTask/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        CalculateAnsShowAsync(5).GetAwaiter().GetResult();
+        Console.ReadKey();
+    }
+    private static ValueTask CalculateAnsShowAsync(int ceiling)
+    {
+        if(ceiling < 0) { return new ValueTask(); }
+        else
+        {
+            return new ValueTask(Task.Run(() =>
+            {
+                Calculator(ceiling);
+            }));
+        }
+
+    }
+
+    private static void Calculator(int ceiling)
+    {
+        int sum = 0;
+        for (int i = 0; i < ceiling; i++)
+            sum += i;
+        Console.WriteLine($"Результат - {sum}. Найден в задаче #{Task.CurrentId}, в потоке #{Thread.CurrentThread.ManagedThreadId}");
+    }
+}
