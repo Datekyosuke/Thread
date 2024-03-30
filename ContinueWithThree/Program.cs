1f138caba8db3820068aa1c04a959a08222273d2
Yury Kuzmin <rakoot@yandex.ru>
2024-03-30 20:21:01 +0300

    continueWithThree

---
+++ ContinueWithThree/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Task<int> task = Task.Run<int>(new Func<int>(GetValue));
+        task.ContinueWith(Increment)
+        .ContinueWith(Increment)
+        .ContinueWith(Increment)
+        .ContinueWith(Increment)
+        .ContinueWith(Increment)
+        .ContinueWith(ShowRes);
+        Console.WriteLine("Main завершил свою работу");
+        Console.ReadKey();
+    }
+    private static int GetValue()
+    {
+        return 10;
+    }
+
+    private static int Increment(Task<int> t)
+    {
+        Console.WriteLine($"Продолжение task ID #{Task.CurrentId}, Thread Id {Thread.CurrentThread.ManagedThreadId}");
+        return t.Result + 1;
+    }
+
+    private static void ShowRes(Task<int> t)
+    {
+        Console.WriteLine($"Продолжение task Id #{Task.CurrentId}. Thread Id #{Thread.CurrentThread.ManagedThreadId}.");
+        Console.WriteLine($"Результат - {t.Result}");
+    }
+}
